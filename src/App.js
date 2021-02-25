da487290f3b274c4bd9ba271b6768fb372e87a47
milliwonkim <rldnjs9347@gmail.com>
2021-02-25 19:14:36 +0900

    [add] add dots and line chart with bar chart

--- src/App.js
+++ src/App.js
@@ -5,7 +5,9 @@ import './App.css'
 function App() {
     const svgRef = useRef()
 
-    let data = [55, 90, 20, 80, 70, 11]
+    let subject = ['korean', 'math', 'english', 'science', 'social']
+    let data = [55, 90, 20, 80, 70]
+    let data2 = [3, 12, 16, 19, 8]
 
     useEffect(() => {
         const margin = { top: 20, right: 100, bottom: 20, left: 100 }
@@ -16,10 +18,11 @@ function App() {
             .select(svgRef.current)
             .attr('width', width)
             .attr('height', height + 100)
+            .attr('position', 'relative')
 
         const xScale = d3
             .scaleBand()
-            .domain(data)
+            .domain(subject)
             .range([0, width - margin.left - margin.right])
             .padding(0.5)
 
@@ -27,6 +30,23 @@ function App() {
             .scaleLinear()
             .domain([0, 100])
             .range([height, margin.top])
+
+        const y2Scale = d3
+            .scaleLinear()
+            .domain([
+                0,
+                d3.max(data2, function (d) {
+                    return d
+                }) + 1,
+            ])
+            .range([height, margin.top])
+
+        const line = d3
+            .line()
+            .x((d) => xScale(subject[data2.indexOf(d)]))
+            .y((d) => y2Scale(d))
+
+        svg.attr('viewBox', [0, 0, width, height])
 
         svg.append('g')
             .attr('class', 'x_bar')
@@ -37,6 +57,26 @@ function App() {
             .attr('class', 'y_bar')
             .attr('transform', 'translate(' + 100 + ',' + -margin.bottom + ')')
             .call(d3.axisLeft(yScale))
+
+        let y2Width = 100 + width - margin.left - margin.right
+
+        svg.append('g')
+            .attr('class', 'y2_bar')
+            .attr('transform', `translate(${y2Width}, ${-margin.bottom})`)
+            .call(d3.axisRight(y2Scale))
+
+        // svg.selectAll('dot')
+        //     .datum(data2)
+        //     .append('circle')
+        //     .attr('r', 3.5)
+        //     .attr('cx', function (d) {
+        //         console.log('cx: ', d)
+        //         return xScale(d)
+        //     })
+        //     .attr('cy', function (d) {
+        //         console.log('cy: ', d)
+        //         return y2Scale(d)
+        //     })
 
         let tooltip = d3
             .select('body')
@@ -50,7 +90,9 @@ function App() {
             .data(data)
             .enter()
             .append('rect')
-            .attr('x', (d) => xScale(d) + margin.left)
+            .attr('x', (d) => {
+                return xScale(subject[data.indexOf(d)]) + margin.left
+            })
             .attr('y', (d) => yScale(d) - margin.bottom)
             .attr('width', (d) => xScale.bandwidth())
             .attr('height', (d) => height - yScale(d))
@@ -58,9 +100,9 @@ function App() {
                 let data = d / 100
                 return d3.interpolateRainbow(data)
             })
-            .on('mouseover', function (d, i) {
-                tooltip.text(i)
-                return tooltip.style('visibility', 'visible')
+            .attr('class', 'bar_chart')
+            .on('mouseover', function (event, data) {
+                return tooltip.text(data).style('visibility', 'visible')
             })
             .on('mousemove', function (d) {
                 return tooltip
@@ -71,7 +113,46 @@ function App() {
                 return tooltip.style('visibility', 'hidden')
             })
 
-        svg.attr('viewBox', [0, 0, width, height])
+        svg.append('path')
+            .datum(data2)
+            .attr('class', 'line_chart')
+            .attr('fill', 'none')
+            .attr('z-index', 999)
+            .attr('stroke', 'red')
+            .attr('stroke-width', 5)
+            .style('position', 'absolute')
+            .style('z-index', 20)
+            .attr('transform', 'translate(' + 125 + ',' + -margin.bottom + ')')
+            .attr('d', line(data2))
+            .attr('class', 'line_chart')
+
+        svg.selectAll('dot')
+            .data(data2)
+            .enter()
+            .append('circle')
+            .attr('class', 'dot')
+            .attr('r', 5)
+            .attr('cx', function (d) {
+                return xScale(subject[data2.indexOf(d)])
+            })
+            .attr('cy', function (d) {
+                return y2Scale(d)
+            })
+            .attr('transform', 'translate(' + 125 + ',' + -margin.bottom + ')')
+            .on('mouseover', function (event, data) {
+                d3.select(this).raise().attr('fill', 'red').attr('r', 10)
+                console.log('data: ', data)
+                return tooltip.text(data).style('visibility', 'visible')
+            })
+            .on('mousemove', function (event) {
+                return tooltip
+                    .style('top', event.pageY - 10 + 'px')
+                    .style('left', event.pageX + 10 + 'px')
+            })
+            .on('mouseout', function () {
+                d3.select(this).attr('fill', 'black').attr('r', 5)
+                return tooltip.style('visibility', 'hidden')
+            })
     }, [])
 
     return (
