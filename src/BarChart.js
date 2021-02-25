d4ab79040bef71d30d6316a9a1aaf699fc40ad25
milliwonkim <rldnjs9347@gmail.com>
2021-02-26 01:23:40 +0900

    [add] add stacked bar chart

---
+++ src/BarChart.js
@@ -0,0 +1,166 @@
+import React, { useEffect, useRef } from 'react'
+import * as d3 from 'd3'
+import './App.css'
+
+function App() {
+    const svgRef = useRef()
+
+    let subject = ['korean', 'math', 'english', 'science', 'social']
+    let data = [55, 90, 20, 80, 70]
+    let data2 = [3, 12, 16, 19, 8]
+
+    useEffect(() => {
+        const margin = { top: 20, right: 100, bottom: 20, left: 100 }
+        const width = 700
+        const height = 700
+
+        const svg = d3
+            .select(svgRef.current)
+            .attr('width', width)
+            .attr('height', height + 100)
+            .attr('position', 'relative')
+
+        const xScale = d3
+            .scaleBand()
+            .domain(subject)
+            .range([0, width - margin.left - margin.right])
+            .padding(0.5)
+
+        const yScale = d3
+            .scaleLinear()
+            .domain([0, 100])
+            .range([height, margin.top])
+
+        const y2Scale = d3
+            .scaleLinear()
+            .domain([
+                0,
+                d3.max(data2, function (d) {
+                    return d
+                }) + 1,
+            ])
+            .range([height, margin.top])
+
+        const line = d3
+            .line()
+            .x((d) => xScale(subject[data2.indexOf(d)]))
+            .y((d) => y2Scale(d))
+
+        svg.attr('viewBox', [0, 0, width, height])
+
+        svg.append('g')
+            .attr('class', 'x_bar')
+            .attr('transform', 'translate(' + margin.left + ',' + 680 + ')')
+            .call(d3.axisBottom(xScale))
+
+        svg.append('g')
+            .attr('class', 'y_bar')
+            .attr('transform', 'translate(' + 100 + ',' + -margin.bottom + ')')
+            .call(d3.axisLeft(yScale))
+
+        let y2Width = 100 + width - margin.left - margin.right
+
+        svg.append('g')
+            .attr('class', 'y2_bar')
+            .attr('transform', `translate(${y2Width}, ${-margin.bottom})`)
+            .call(d3.axisRight(y2Scale))
+
+        // svg.selectAll('dot')
+        //     .datum(data2)
+        //     .append('circle')
+        //     .attr('r', 3.5)
+        //     .attr('cx', function (d) {
+        //         console.log('cx: ', d)
+        //         return xScale(d)
+        //     })
+        //     .attr('cy', function (d) {
+        //         console.log('cy: ', d)
+        //         return y2Scale(d)
+        //     })
+
+        let tooltip = d3
+            .select('body')
+            .append('div')
+            .style('position', 'absolute')
+            .style('z-index', 10)
+            .style('visibility', 'hidden')
+            .style('background', 'white')
+
+        svg.selectAll('rect')
+            .data(data)
+            .enter()
+            .append('rect')
+            .attr('x', (d) => {
+                return xScale(subject[data.indexOf(d)]) + margin.left
+            })
+            .attr('y', (d) => yScale(d) - margin.bottom)
+            .attr('width', (d) => xScale.bandwidth())
+            .attr('height', (d) => height - yScale(d))
+            .attr('fill', function (d) {
+                let data = d / 100
+                return d3.interpolateRainbow(data)
+            })
+            .attr('class', 'bar_chart')
+            .on('mouseover', function (event, data) {
+                return tooltip.text(data).style('visibility', 'visible')
+            })
+            .on('mousemove', function (d) {
+                return tooltip
+                    .style('top', d.pageY - 10 + 'px')
+                    .style('left', d.pageX + 10 + 'px')
+            })
+            .on('mouseout', function () {
+                return tooltip.style('visibility', 'hidden')
+            })
+
+        svg.append('path')
+            .datum(data2)
+            .attr('class', 'line_chart')
+            .attr('fill', 'none')
+            .attr('z-index', 999)
+            .attr('stroke', 'red')
+            .attr('stroke-width', 5)
+            .style('position', 'absolute')
+            .style('z-index', 20)
+            .attr('transform', 'translate(' + 125 + ',' + -margin.bottom + ')')
+            .attr('d', line(data2))
+            .attr('class', 'line_chart')
+
+        svg.selectAll('dot')
+            .data(data2)
+            .enter()
+            .append('circle')
+            .attr('class', 'dot')
+            .attr('r', 5)
+            .attr('cx', function (d) {
+                return xScale(subject[data2.indexOf(d)])
+            })
+            .attr('cy', function (d) {
+                return y2Scale(d)
+            })
+            .attr('transform', 'translate(' + 125 + ',' + -margin.bottom + ')')
+            .on('mouseover', function (event, data) {
+                d3.select(this).raise().attr('fill', 'red').attr('r', 10)
+                console.log('data: ', data)
+                return tooltip.text(data).style('visibility', 'visible')
+            })
+            .on('mousemove', function (event) {
+                return tooltip
+                    .style('top', event.pageY - 10 + 'px')
+                    .style('left', event.pageX + 10 + 'px')
+            })
+            .on('mouseout', function () {
+                d3.select(this).attr('fill', 'black').attr('r', 5)
+                return tooltip.style('visibility', 'hidden')
+            })
+    }, [])
+
+    return (
+        <div className="App">
+            <h1>Bar Chart</h1>
+            <svg ref={svgRef} />
+        </div>
+    )
+}
+
+export default App
---
+++ src/StackedBarChart.js
@@ -0,0 +1,140 @@
+import React, { useState, useEffect, useRef } from 'react'
+import * as d3 from 'd3'
+import './App.css'
+import { stackOrderAscending } from 'd3'
+
+function StackedBarChart() {
+    const svgRef = useRef()
+
+    const data = [
+        {
+            year: 1980,
+            '🥑': 10,
+            '🍌': 20,
+            '🍆': 30,
+        },
+        {
+            year: 1990,
+            '🥑': 20,
+            '🍌': 40,
+            '🍆': 60,
+        },
+        {
+            year: 2000,
+            '🥑': 30,
+            '🍌': 45,
+            '🍆': 80,
+        },
+        {
+            year: 2010,
+            '🥑': 40,
+            '🍌': 60,
+            '🍆': 100,
+        },
+        {
+            year: 2020,
+            '🥑': 50,
+            '🍌': 80,
+            '🍆': 120,
+        },
+    ]
+
+    const allKeys = ['🥑', '🍌', '🍆']
+
+    const colors = {
+        '🥑': 'green',
+        '🍌': 'orange',
+        '🍆': 'purple',
+    }
+
+    const [keys, setKeys] = useState(allKeys)
+
+    useEffect(() => {
+        const margin = { top: 20, right: 100, bottom: 20, left: 100 }
+        const width = 600
+        const height = 400
+
+        const stackGenerator = d3.stack().keys(keys).order(stackOrderAscending)
+
+        const layers = stackGenerator(data)
+
+        const extent = [
+            0,
+            d3.max(layers, (layer) => d3.max(layer, (d) => d[1])),
+        ]
+
+        const svg = d3
+            .select(svgRef.current)
+            .attr('viewBox', [0, 0, width, height - 100])
+
+        const xScale = d3
+            .scaleBand()
+            .domain(data.map((d) => d.year))
+            .range([0, width - 180])
+            .padding(0.3)
+
+        const yScale = d3
+            .scaleLinear()
+            .domain(extent)
+            .range([height - 100, 0])
+
+        let xHeight = height - margin.bottom - 100
+
+        svg.append('g')
+            .attr('class', 'x_bar')
+            .call(d3.axisBottom(xScale))
+            .attr('transform', 'translate(' + margin.left + ',' + xHeight + ')')
+
+        svg.append('g')
+            .attr('class', 'y_bar')
+            .call(d3.axisLeft(yScale))
+            .attr('transform', 'translate(' + 100 + ',' + -margin.bottom + ')')
+
+        svg.selectAll('.layer')
+            .data(layers)
+            .join('g')
+            .attr('class', 'layer')
+            .attr('fill', (layer) => colors[layer.key])
+            .selectAll('rect')
+            .data((layer) => layer)
+            .join('rect')
+            .attr('x', (sequence) => xScale(sequence.data.year))
+            .attr('y', (sequence) => yScale(sequence[1]))
+            .attr('width', xScale.bandwidth())
+            .attr(
+                'height',
+                (sequence) => yScale(sequence[0]) - yScale(sequence[1])
+            )
+            .attr('transform', 'translate(' + 100 + ',' + -margin.bottom + ')')
+
+        return () => {
+            svg.selectAll('g').remove()
+        }
+    }, [colors, data, keys])
+
+    return (
+        <div className="App">
+            <h1>StackedBarChart</h1>
+            <svg ref={svgRef} />
+            {allKeys.map((key) => (
+                <div key={key} className="field">
+                    {key}
+                    <input
+                        id={key}
+                        type="checkbox"
+                        checked={keys.includes(key)}
+                        onChange={(e) => {
+                            if (e.target.checked) {
+                                setKeys(Array.from(new Set([...keys, key])))
+                            } else {
+                                setKeys(keys.filter((_key) => _key !== key))
+                            }
+                        }}
+                    />
+                </div>
+            ))}
+        </div>
+    )
+}
+
+export default StackedBarChart
